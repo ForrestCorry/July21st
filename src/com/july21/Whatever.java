9ad4a0362d2de08e9ba536164a6f1cc6abf93e53
Forrest <forrest.corry@gmail.com>
2016-07-21 10:26:07 -0400

    initial commit

---
+++ src/com/july21/Whatever.java
@@ -0,0 +1,12 @@
+package com.july21;
+
+public class Whatever {
+
+	public static void main(String[] args) 
+	// I don't understand the ! statements and how they interact with the &, |
+	
+	{
+		
+	}
+
+}
